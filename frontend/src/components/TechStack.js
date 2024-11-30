9e9e9dd8b2271d153bd89fffd9cfcb9350413901
Maciej Krzywicki <maciastydev>
2024-11-30 21:05:22 +0100

    Typo

--- frontend/src/components/TechStack.js
+++ frontend/src/components/TechStack.js
@@ -27,7 +27,7 @@ const TechStack = () => {
 
   return (
     <div className="mx-5 sm:mx-8 lg:mx-12 mb-10 lg:mb-16">
-      <div className="m-5 p-6 bg-customBlack4 rounded-lgmax-w-7xl mx-auto lg:p-11 shadow-lg shadow-customBlack6">
+      <div className="m-5 p-6 bg-customBlack4 rounded-lg max-w-7xl mx-auto lg:p-11 shadow-lg shadow-customBlack6">
         <h2 className="text-2xl lg:text-3xl font-bold text-customWhite87 mb-4 lg:mb-8">
           Tech stack
         </h2>
