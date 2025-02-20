760772cf541dd1d34f11d75af7fc23c80c5634f9
mac-v <maciastydev@gmail.com>
2025-02-20 15:41:37 +0100

    manage.py modified

--- backend/manage.py
+++ backend/manage.py
@@ -5,16 +5,19 @@ import sys
 from dotenv import load_dotenv
 
 def main():
-    env_file = '.env.prod' if os.getenv('ENV') == 'production' else '.env'
-    print(f"Loading env file: {env_file}")
 
-    load_dotenv(env_file, override=True)
-    print(f"ENV={os.getenv('ENV')}")
-    print(f"DATABASE_URL={os.getenv('DATABASE_URL')}")
+    is_vercel = os.getenv('VERCEL') is not None
+
+    if not is_vercel:
+        env_file = '.env.prod' if os.getenv('ENV') == 'production' else '.env.local'
+        print(f"⚡ Loading env file: {env_file}")
+        load_dotenv(env_file, override=True)
+
+    print(f"🔹 ENV={os.getenv('ENV')}")
+    print(f"🔹 DATABASE_URL={os.getenv('DATABASE_URL')}")
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -24,7 +27,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
