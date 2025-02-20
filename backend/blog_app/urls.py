179770a9226f05d2e004bf9233e41e0252d73ac6
mac-v <maciastydev@gmail.com>
2025-02-20 12:47:17 +0100

    vercel django serverless template

--- backend/blog_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import submit_form
-from .views import hello_world
-urlpatterns = [
-    path('form', submit_form, name="submit-form"),
-    path('hello',hello_world, name="hello-world"),
-]
