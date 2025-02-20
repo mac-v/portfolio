b79062f5d493ed24b34c3c50997a4eb622dc799f
mac-v <maciastydev@gmail.com>
2025-02-20 17:37:17 +0100

    typo fix

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -33,7 +33,7 @@ const MyForm = () => {
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://portfolio-qly6.vercel.app/blog_app/form', {
+      const response = await fetch('https://portfolio-qly6.vercel.app/blog_app/form', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
